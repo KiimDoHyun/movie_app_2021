2e248d81e64c2290bc48e0a9eb82b0da17437007
KDH20160140 <bzidol@naver.com>
2021-01-29 11:25:26 +0900

    css 작업중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './movie';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//movie api https://yts-proxy.now.sh/list_movies.json
+// https://yts.ma/api/v2 대신 https://yts-proxy.now.sh/ 로 사용
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  //async : 비동기로 처리할거라는 뜻.
+  //await : 뭘 기다릴건지
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // .data.data.movies 로 접근하는것을 위의 코드를 통해 줄이는 것이 가능.
+    this.setState({ movies, isLoading: false });
+    console.log(movies);
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>)
+        }
+      </section>
+    )
+  }
 }
+
+//리액트는 자동적으로 모든 class 의 render 메소드를 수행한다.
 
 export default App;
